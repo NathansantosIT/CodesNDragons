4901c80982399c83f0915971c8beeeb22b2f5452
Karen <Karen@DESKTOP-DV07IOR.home>
2018-07-01 22:36:42 -0300

    01/07 22:36 Karen Video 18 utils

--- Game/src/States/GameState.java
+++ Game/src/States/GameState.java
@@ -5,23 +5,28 @@ import java.awt.Graphics;
 import Entitys.Players;
 import Tiles.Tile;
 import backEnd_game.Game;
+import worlds.World;
 
 public class GameState extends State {
 	
 	private Players player;
+	private World world;
 	
 	public GameState(Game game) {
 		super(game);
 		player = new Players(game, 480, 600);
+		world = new World("res/worlds/worlds1.txt");
 	}
 	
 	public void tick() {
+		world.tick();
 		player.tick();
+		
 	}
 	
 	public void render(Graphics g) {
-		player.render(g);
-		Tile.tiles[1].render(g, 0, 0);
+		world.render(g);
+		player.render(g);		
 	}
 
 }
--- Game/src/worlds/World.java
+++ Game/src/worlds/World.java
@@ -2,6 +2,8 @@ package worlds;
 
 import java.awt.Graphics;
 
+import Tiles.Tile;
+
 public class World {
 	
 	private int width, height;
@@ -16,11 +18,28 @@ public class World {
 	}
 	
 	public void render(Graphics g) {
+		for(int y = 0;y < height;y++) {
+			for(int x = 0;x < width;x++) {
+				getTile(x,y).render(g, x*Tile.TILEWIDTH, y*Tile.TILEHEIGHT);
+			}
+		}
 		
+	}
+	public Tile getTile(int x,int y) {
+		Tile t = Tile.tiles[tiles[x][y]];
+		if (t == null)
+			return Tile.dirtTile;
+		return t;
 	}
 	private void loadWorld(String path) {
 		width = 5;
 		height = 5;
 		tiles = new int[width][height];
+		
+		for (int x = 0;x < width;x++) {
+			for(int y = 0;y < height;y++) {
+				tiles[x][y] = 0;
+			}
+		}
 	}
 }
